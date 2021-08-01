75f33bb0a247b2245318fba26cd82e220f57497a
CheshireCode <50378606+CheshirCode@users.noreply.github.com>
2021-08-01 20:10:46 +0300

    begin

---
+++ js/script.js
@@ -0,0 +1,15 @@
+const menuButton = document.querySelector(".header__burger");
+const menuButton1 = document.querySelector(".nav");
+const contentBody = document.querySelector("body");
+
+menuButton.onclick = function () {
+  menuButton.classList.toggle("active");
+  menuButton1.classList.toggle("active");
+  contentBody.classList.toggle("lock");
+}
+
+menuButton1.onclick = function () {
+  menuButton.classList.remove("active");
+  menuButton1.classList.remove("active");
+  contentBody.classList.remove("lock");
+}
