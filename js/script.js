193ff4ee9344c528d759a5602c9f779e8aa519f0
CheshireCode <50378606+CheshirCode@users.noreply.github.com>
2021-08-02 20:45:58 +0300

    add slider, adaptive

--- js/script.js
+++ js/script.js
@@ -13,3 +13,22 @@ menuButton1.onclick = function () {
   menuButton1.classList.remove("active");
   contentBody.classList.remove("lock");
 }
+
+function slidesPlugin(activeSlide) {
+  const slides = document.querySelectorAll('.slide');
+  slides[activeSlide].classList.add('active')
+
+  for (const slide of slides) {
+    slide.addEventListener('click', () => {
+      clearActiveClasses()
+      slide.classList.add('active')
+    })
+  }
+
+  function clearActiveClasses() {
+    slides.forEach((slide) => {
+      slide.classList.remove('active')
+    })
+  }
+}
+slidesPlugin(0)
